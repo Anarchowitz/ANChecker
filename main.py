5eab551eb42403089587c9f0dd3888e042ad07ac
Anarch <123408756+Anarchowitz@users.noreply.github.com>
2024-03-10 18:22:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,130 @@
 import os
-import ctypes
+import webbrowser
+import keyboard
+import pydirectinput as pyd
 from time import sleep
-def clearscreen():
-    os.system('cls & title HelinChecker')
+def greetings():
+    os.system('cls & title ANChecker')
+    print("""
+ ▄▄▄      ███▄    █  ▄████▄   ██░ ██ ▓█████ ▄████▄  ▀██ ▄█▀▓█████ ██▀███  
+▒████▄    ██ ▀█   █ ▒██▀ ▀█ ▒▓██░ ██ ▓█   ▀▒██▀ ▀█   ██▄█▒ ▓█   ▀▓██ ▒ ██▒
+▒██  ▀█▄ ▓██  ▀█ ██▒▒▓█    ▄░▒██▀▀██ ▒███  ▒▓█    ▄ ▓███▄░ ▒███  ▓██ ░▄█ ▒
+░██▄▄▄▄██▓██▒  ▐▌██▒▒▓▓▄ ▄██ ░▓█ ░██ ▒▓█  ▄▒▓▓▄ ▄██ ▓██ █▄ ▒▓█  ▄▒██▀▀█▄  
+ ▓█   ▓██▒██░   ▓██░▒ ▓███▀  ░▓█▒░██▓░▒████▒ ▓███▀  ▒██▒ █▄░▒████░██▓ ▒██▒
+ ▒▒   ▓▒█░ ▒░   ▒ ▒ ░ ░▒ ▒    ▒ ░░▒░▒░░ ▒░ ░ ░▒ ▒   ▒ ▒▒ ▓▒░░ ▒░ ░ ▒▓ ░▒▓░
+  ░   ▒▒ ░ ░░   ░ ▒░  ░  ▒    ▒ ░▒░ ░ ░ ░    ░  ▒   ░ ░▒ ▒░ ░ ░    ░▒ ░ ▒░
+  ░   ▒     ░   ░ ░ ░         ░  ░░ ░   ░  ░        ░ ░░ ░    ░     ░   ░ 
+      ░           ░ ░ ░       ░  ░  ░   ░  ░ ░      ░  ░      ░     ░     
+    Actually build v1.0.2 (10.03.2024)
+    offical github - https://github.com/Anarchowitz/ANChecker
+    """)
+    sleep(2.6)
+    os.system('pause')
+    main()
 def main():
-    clearscreen()
+    os.system('cls')
     try:
-        print('HELIN PROJECT')
+        print('Main Menu')
         print("""
-        1. Открыть папки (recent/appdata/temp и т.д)
-        2. Программы для проверки
+        1. Проверка в игре. (авто-прожатие INSERT и т.д)
+        2. Открыть папки (recent/appdata/temp и т.д)
+        3. Программы для проверки (LastActivity и т.д)
+        4. Открытие популярных сайтов
+        [5]. Апдейт Логи + Контакт with dev
         """)
         ans = input("Выбрано: ")
         if ans == '1':
-            path = 'C:/HelinChecker/assets'
-            os.startfile(path + '/fileopenner.bat')
-            return main()
+            gamecheck()
         elif ans == '2':
+            script_dir = os.path.dirname(os.path.abspath(__file__))
+            print(script_dir)
+            file_path = os.path.join(script_dir, 'assets', 'fileopenner.bat')
+            print(file_path)
+            os.system('pause')
+            os.startfile(file_path)
+            main()
+        elif ans == '3':
             appscheck()
+        elif ans == '4':
+            os.system('cls')
+            print('Открываю сайты... \n УСПЕШНО!')
+            webbrowser.open('http://oplata.info')
+            webbrowser.open('http://midnight.im')
+            webbrowser.open('http://neverlose.cc')
+            webbrowser.open('http://xone.fun')
+            print('[Сообщение проверяющему]: Проверьте почту/вк у подозреваемого!')
+            os.system('pause & cls'); main()
+        elif ans == '5':
+            os.system('cls')
+            print('UPDATE LOG!')
+            print("""
+        Update season 1:
+            07.03.2024 -> Release version! (v:1.0.0)
+            08.03.2024 -> Remove useless function and false detection virus. (v:1.0.1)
+            09.03.2024 -> 
+                  [V] Correcting the path to the asset folder.
+                  [V] Add new signatures to search for cheats
+                  [V] Added new "CheatCheck" function
+            10.03.2024 -> (v:1.0.2)
+                  [V] Some minor fixes...
+                  [V] Removed the dependency on the "Assets" folder!
+                  RELEASING VERSION: 1.0.2
+        Update season 2:
+            soon! (gui+new features )   
+(что бы вернуться назад нажмите любую кнопку)
+        """)
+            sleep(1.3)
+            os.system('pause & cls')
+            main()
         else:
-            clearscreen()
+            os.system('cls')
             for i in range(5):
                 print('[ERROR] НАПИШИТЕ ЦИФРУ! (1-2)')
             sleep(3)
-            clearscreen()
+            os.system('cls')
             main()
     except KeyboardInterrupt:
-        os._exit(1)
+        os.exit(1)
+active=False
+def gamecheck():  # нажатие клавиш в игре  - ПОЛНЫЙ ГОВНОКОД НА КОСТЫЛЯХ
+    os.system('cls')
+    print('Pressing keys in game')
+    def toggle_activation():
+        global active
+        active = not active
+        if active:
+            print('Начинаем цикл прожатия.')
+        else:
+            print('Цикл остановлен/Завершен!')
+            os.system('pause & cls')
+            main()
+    keyboard.add_hotkey('insert', toggle_activation)
+    keys_to_press = [
+        ('decimal', 'del'),
+        'fn',
+        'home',
+        ('ctrlleft', 'ctrlright'),
+        'end',
+        'pagedown',
+        'pageup',
+        'shiftright',
+        'shiftleft',
+        'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9', 'f10', 'f11', 'f12', 'insert'
+    ]
+    def keyspressing():
+        if active:
+                for key in keys_to_press:
+                    if isinstance(key, str):
+                        pyd.press(key)
+                        print(f'Нажата клавиша: {key}')
+                    else:
+                        pyd.press(key[0])
+                        print(f'Нажата клавиша: {key[1]}')
+    while True:
+        sleep(1)
+        keyspressing()
 def appscheck():
-    path = 'C:/HelinChecker/assets'
+    path = 'assets'
     apps = {
         '1': 'Lastactivityview/LastActivityView',
         '2': 'Everything/Everything',
@@ -37,10 +133,11 @@ def appscheck():
         '5': 'HxD/HxD',
         '6': 'Userassistview/Userassistview',
         '7': 'Usbdeview/UsbDeview',
-        '8': 'Shellbags/Shellbags'
+        '8': 'Shellbags/Shellbags',
+        '9': 'BrowserDownloadsView/BrowserDownloadsView'
     }
-    clearscreen()
-    print('HELIN PROJECT')
+    os.system('cls')
+    print('Programs Menu')
     print("""
     1. Lastactivityview
     2. Everything
@@ -50,33 +147,25 @@ def appscheck():
     6. Userassistview
     7. Usbdeview
     8. ShellbagsAnalyzer
+    9. BrowserDownloadsView
     ----------------------
-    9. <- ВЕРНУТЬСЯ НАЗАД!
+    10. <- ВЕРНУТЬСЯ НАЗАД!
     """)
     ans = input("Выбрано: ")
-    if ans == '9':
+    if ans == '10':
         main()
     elif ans in apps:
-        os.startfile(os.path.join(path, apps[ans]))
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        program_path = os.path.join(script_dir, 'assets', apps[ans])
+        os.startfile(program_path)
         sleep(1)
-        clearscreen()
+        os.system('cls')
         appscheck()
     else:
         for _ in range(5):
-            print('[ERROR] НАПИШИТЕ ЦИФРУ! (1-7)')
+            print('[ERROR] НАПИШИТЕ ЦИФРУ! (1-10)')
         sleep(3)
-        clearscreen()
+        os.system('cls')
         appscheck()
-def is_admin():
-   try:
-     return os.getuid() == 0
-   except AttributeError:
-     return ctypes.windll.shell32.IsUserAnAdmin() != 0
 if __name__ == "__main__":
-    if is_admin() == True:
-        main()
-    else:
-        for i in range(5):
-            print("[ERROR] ЗАПУСТИТЕ ФАЙЛ С ПРАВАМИ АДМИНИСТРАТОРА")
-        sleep(3)
-        os._exit(1)
+        greetings()
